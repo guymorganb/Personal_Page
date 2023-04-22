27b9e8fadb7c35d2ddb91c25337b7db7136464d5
guymorganb <guymorganb@gmail.com>
2023-04-21 23:48:30 -0500

    first commit

---
+++ myPageAssets/rolloverscript.js
@@ -0,0 +1,31 @@
+'use strict'
+
+
+
+"use strict";
+$(document).ready(() => {
+
+    // process each img tag
+    $("#image_rollovers img").each( (index, img) => {
+        const oldURL = img.src;      // gets the src attribute
+        const newURL = img.id;       // gets the id attribute
+
+        // preload rollover image		
+        const rolloverImage = new Image();
+        rolloverImage.src = newURL;
+
+         //set up event handlers for hovering over an image
+        $("#image_rollovers img").hover(   // use jQuery syntax to access hover() method
+            () => { $("#image_rollovers img").css({ transition: "all 1s ease-in-out", transformStyle: "preserve-3d", transform: "rotateY(180deg)" })
+                img.src = newURL  // hover over
+            },
+           () => { $("#image_rollovers img").css({ transition: "all 0.5s ease-in-out", transformStyle: "preserve-3d", transform: "rotateY(0deg)"})
+            img.src = oldURL   // hover out
+           }
+        ); 
+        $("#image_rollovers img").mouseover( 
+            () => img.src = newURL )
+        $("#image_rollovers img").mouseout( 
+            () => img.src = oldURL)
+    });
+});
---
+++ myPageAssets/scroll.js
@@ -0,0 +1,36 @@
+let path = document.querySelector('path');
+let pathLength = path.getTotalLength()
+
+path.style.strokeDasharray = pathLength + ' ' + pathLength;
+
+path.style.strokeDashoffset = pathLength;
+
+window.addEventListener('scroll', () => {
+    
+    //What % down is it
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+    //length to offset the dashes
+    var drawLength = pathLength * scrollPercentage;
+
+    //draw in reverse
+    path.style.strokeDashoffset = pathLength - drawLength;
+
+    //create parallax effect for any element with scroll class
+const target = document.querySelectorAll('.scroll');
+
+var index = 0;
+var length = target.length;
+for (index; index < length; index++){
+    var pos = window.pageYOffset * target[index].dataset.rate;
+
+    if(target[index].dataset.direction === 'vertical'){
+        target[index].style.transform = 'translate3d(0px,' +pos+'px, 0px)';
+    } else {
+        var posX = window.pageYOffset * target[index].dataset.ratex;
+        var posY = window.pageYOffset * target[index].dataset.ratey;
+        
+        target[index.style.transform] ='translate3d('+posX+'px, '+posY+'px, 0px)';
+    }
+}
+})
